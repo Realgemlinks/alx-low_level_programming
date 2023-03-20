731fa48daf6fb6b5ca2cb03fdcec56a6a23c4b28
Oloyede Elijah Oluwaseun <elijaholoyede@yahoo.com>
2023-03-20 14:57:34 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,10 +11,9 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter != 'q')
-
 			putchar(letter);
-				putchar('\n');
 	}
+
 	putchar('\n');
 
 	return (0);
