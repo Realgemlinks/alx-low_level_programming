3ea14acf92ca80a4a3cb57e8dfba449f8e862fba
Oloyede Elijah Oluwaseun <elijaholoyede@yahoo.com>
2023-03-22 04:28:03 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,21 @@
 nclude "main.h"
 /**
  * print_alphabet_x10 - make alphabet x10 times
- *
  * Return: void
  */
+
 void print_alphabet_x10(void)
 {
 	char c;
 	int i = 0;
 	
 	while (i < 10)
-        {
+	{
 	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
 	}
-	i++;
 	_putchar('\n');
+	i++;
 	}
 }
-
