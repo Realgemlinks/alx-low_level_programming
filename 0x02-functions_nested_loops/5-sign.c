665cd841b459f07c21fa0c839ded9d28244db58b
Oloyede Elijah Oluwaseun <elijaholoyede@yahoo.com>
2023-03-22 05:44:19 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,17 +8,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar (41);
+	_putchar(41);
 	return (1);
 	}
-	else if (n <  0)
+	else if (n < 0)
 	{
-	_putchar (45);
+	_putchar(45);
 	return (-1);
 	}
 	else
 	{
-	_putchar (48);
+	_putchar(48);
 	return (0);
 	}
 }
